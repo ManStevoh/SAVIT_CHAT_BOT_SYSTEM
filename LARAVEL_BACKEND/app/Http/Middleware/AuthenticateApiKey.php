7dc9350b74469fe755ad964324cc24fed4700514
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:52 +0300

    Add AuthenticateApiKey.php (part 2/6) in LARAVEL_BACKEND\app\Http\Middleware

--- LARAVEL_BACKEND/app/Http/Middleware/AuthenticateApiKey.php
+++ LARAVEL_BACKEND/app/Http/Middleware/AuthenticateApiKey.php
@@ -4,3 +4,9 @@ namespace App\Http\Middleware;
 
 use App\Services\Platform\ApiKeyService;
 use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateApiKey
+{
+    public function __construct(
