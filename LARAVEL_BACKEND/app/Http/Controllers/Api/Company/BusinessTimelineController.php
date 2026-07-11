1f56964fd17747aa41830de7edba5173b1415651
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:01:20 +0300

    Add BusinessTimelineController.php (part 2/8) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/BusinessTimelineController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/BusinessTimelineController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Services\Agent\Timeline\BusinessTimelineService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BusinessTimelineController extends Controller
+{
+    public function index(Request $request, BusinessTimelineService $timeline): JsonResponse
