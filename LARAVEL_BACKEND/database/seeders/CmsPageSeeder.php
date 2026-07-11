b0a8adb5dfc650cbe28ea45ef78ba99bb93de34e
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:23:11 +0300

    Add CmsPageSeeder.php (part 2/78) in LARAVEL_BACKEND\database\seeders

--- LARAVEL_BACKEND/database/seeders/CmsPageSeeder.php
+++ LARAVEL_BACKEND/database/seeders/CmsPageSeeder.php
@@ -4,3 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\CmsPage;
 use App\Models\CmsSection;
+use App\Models\LandingFaq;
+use App\Models\Testimonial;
+use Illuminate\Database\Seeder;
+
+class CmsPageSeeder extends Seeder
+{
