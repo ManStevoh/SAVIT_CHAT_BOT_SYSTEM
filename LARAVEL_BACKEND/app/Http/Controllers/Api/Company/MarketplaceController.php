c03c1351f6e6f5689bc47d8ba6b91b4d70e8bf18
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:19 +0300

    Add MarketplaceController.php (part 2/12) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/MarketplaceController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/MarketplaceController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Services\Agent\Platform\MarketplaceModuleService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use InvalidArgumentException;
+
+class MarketplaceController extends Controller
+{
