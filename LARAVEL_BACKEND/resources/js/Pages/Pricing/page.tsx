ef40844c170c67b01f98d6410ac641cc21aef88b
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:30:27 +0300

    Add page.tsx (part 1/1) in LARAVEL_BACKEND\resources\js\Pages\Pricing

---
+++ LARAVEL_BACKEND/resources/js/Pages/Pricing/page.tsx
@@ -0,0 +1,5 @@
+﻿import { LandoCmsPage } from "@/components/lando/cms-page"
+
+export default function PricingPage() {
+  return <LandoCmsPage slug="pricing" fallbackTitle="Pricing — Essem Chat" />
+}
