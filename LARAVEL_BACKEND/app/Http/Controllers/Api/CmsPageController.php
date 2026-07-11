574350a926306fb7ed87ade18865f8a7321ce401
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:22:08 +0300

    Add CmsPageController.php (part 4/16) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/CmsPageController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/CmsPageController.php
@@ -16,3 +16,9 @@ class CmsPageController extends Controller
     {
         $page = CmsPage::where('slug', $slug)->where('is_published', true)->first();
 
+        if (! $page) {
+            return response()->json(['message' => 'Page not found'], 404);
+        }
+
+        $page->load('sections');
+
