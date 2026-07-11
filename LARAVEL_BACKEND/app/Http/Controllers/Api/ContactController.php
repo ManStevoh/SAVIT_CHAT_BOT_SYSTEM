cdcd4bef4395030ac9d0a5a8398cb31114a36e21
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:22:34 +0300

    Add ContactController.php (part 3/7) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/ContactController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/ContactController.php
@@ -10,3 +10,9 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    public function store(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|string|max:5000',
