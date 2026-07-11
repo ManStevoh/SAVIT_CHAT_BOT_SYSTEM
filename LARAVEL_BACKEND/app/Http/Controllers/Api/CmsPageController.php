0516f24b9062e6020b413d0f3b26c9e134593c01
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:22:01 +0300

    Add CmsPageController.php (part 2/16) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/CmsPageController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/CmsPageController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\CmsPage;
+use App\Models\LandingFaq;
+use App\Models\PlatformSetting;
+use App\Models\Testimonial;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+
