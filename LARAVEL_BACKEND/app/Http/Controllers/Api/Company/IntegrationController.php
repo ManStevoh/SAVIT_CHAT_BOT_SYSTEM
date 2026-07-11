f771b3bd7915f3fe5db4242a5dcbf13685ccbe80
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:01:43 +0300

    Add IntegrationController.php (part 2/21) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/IntegrationController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/IntegrationController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Models\CompanyIntegration;
+use App\Services\Agent\Integrations\ConnectorRegistry;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class IntegrationController extends Controller
+{
