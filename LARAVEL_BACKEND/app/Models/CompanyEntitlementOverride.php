f068273a032482604d79219c85c585546d8784fc
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:31 +0300

    Add CompanyEntitlementOverride.php (part 2/4) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/CompanyEntitlementOverride.php
+++ LARAVEL_BACKEND/app/Models/CompanyEntitlementOverride.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyEntitlementOverride extends Model
+{
+    protected $fillable = ['company_id', 'overrides', 'notes'];
+
+    protected $casts = [
