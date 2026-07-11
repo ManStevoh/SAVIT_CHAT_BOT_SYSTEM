2915f28ac998d582b740f69eb88689734faddae2
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:35 +0300

    Add PolicyRuleController.php (part 3/16) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/PolicyRuleController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/PolicyRuleController.php
@@ -10,3 +10,9 @@ use Illuminate\Http\Request;
 
 class PolicyRuleController extends Controller
 {
+    public function index(Request $request): JsonResponse
+    {
+        $company = $request->user()->company;
+        if (! $company) {
+            return response()->json(['message' => 'No company.'], 403);
+        }
