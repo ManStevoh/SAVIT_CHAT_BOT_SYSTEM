b8509fc0472fe34b779d1917901109e0103f5c7a
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:28 +0300

    Add CompanyApiKey.php (part 2/6) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/CompanyApiKey.php
+++ LARAVEL_BACKEND/app/Models/CompanyApiKey.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyApiKey extends Model
+{
+    protected $fillable = [
+        'company_id', 'name', 'key_prefix', 'key_hash', 'scopes', 'last_used_at', 'revoked_at', 'created_by',
+    ];
