34020338423d7e714bfa6b66fa716b7140390997
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:24:37 +0300

    Add cms-page.tsx (part 1/14) in LARAVEL_BACKEND\resources\js\components\lando

---
+++ LARAVEL_BACKEND/resources/js/components/lando/cms-page.tsx
@@ -0,0 +1,6 @@
+﻿"use client"
+
+import { Head } from "@inertiajs/react"
+import { usePathname } from "next/navigation"
+import { LandoNavbar } from "./navbar"
+import { LandoFooter } from "./footer"
