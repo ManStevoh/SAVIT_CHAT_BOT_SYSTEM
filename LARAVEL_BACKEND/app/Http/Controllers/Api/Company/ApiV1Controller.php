14c00ff63e4277bd2c145329b2c8e1d28f6fe75a
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:01:08 +0300

    Add ApiV1Controller.php (part 2/6) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/ApiV1Controller.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/ApiV1Controller.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+ * Read-only API v1 surface authenticated via company API keys.
+ */
