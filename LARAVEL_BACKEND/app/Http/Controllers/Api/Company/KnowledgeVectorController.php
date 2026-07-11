d697ade06ebbc64fd037c6396506fc46f46cd9d0
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:17 +0300

    Add KnowledgeVectorController.php (part 2/4) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/KnowledgeVectorController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/KnowledgeVectorController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Services\AI\KnowledgeChunkService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class KnowledgeVectorController extends Controller
+{
+    public function status(Request $request, KnowledgeChunkService $chunks): JsonResponse
