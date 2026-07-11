528f8e85b853ccd9e10f5c28f7904013581c133a
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:52 +0300

    Add NotificationTemplate.php (part 2/3) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/NotificationTemplate.php
+++ LARAVEL_BACKEND/app/Models/NotificationTemplate.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+class NotificationTemplate extends Model
+{
+    protected $fillable = [
+        'key', 'channel', 'title', 'body_template', 'type', 'variables', 'is_active',
+    ];
+
