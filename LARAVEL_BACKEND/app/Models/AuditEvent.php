fe0d505c2255ffc979a9d3d2eba816927c01dc6b
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:08 +0300

    Add AuditEvent.php (part 2/7) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/AuditEvent.php
+++ LARAVEL_BACKEND/app/Models/AuditEvent.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class AuditEvent extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
