4260b196222f6c34e8037ad4c1880e7d94bf405d
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:25:39 +0300

    Add footer.tsx (part 1/10) in LARAVEL_BACKEND\resources\js\components\lando

---
+++ LARAVEL_BACKEND/resources/js/components/lando/footer.tsx
@@ -0,0 +1,6 @@
+﻿import Link from "next/link"
+import { AppLogoAndName } from "@/components/branding/AppLogoAndName"
+import type { CmsLink } from "./types"
+
+interface LandoFooterProps {
+  copyright?: string
