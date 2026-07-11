d257bb48006e5dd0b20d7155a7e606c9f1485c19
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:58 +0300

    Add ProcessTrialTransitionsJob.php (part 2/10) in LARAVEL_BACKEND\app\Jobs\Platform

--- LARAVEL_BACKEND/app/Jobs/Platform/ProcessTrialTransitionsJob.php
+++ LARAVEL_BACKEND/app/Jobs/Platform/ProcessTrialTransitionsJob.php
@@ -4,3 +4,9 @@ namespace App\Jobs\Platform;
 
 use App\Models\Plan;
 use App\Models\Subscription;
+use App\Services\Platform\DomainEventDispatcher;
+use App\Services\Platform\NotificationDispatcher;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
