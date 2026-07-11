a29de755c789a2528552cbfaf04c1409f9f138b8
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:50 +0300

    Add NotificationDelivery.php (part 2/4) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/NotificationDelivery.php
+++ LARAVEL_BACKEND/app/Models/NotificationDelivery.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class NotificationDelivery extends Model
+{
+    protected $fillable = [
+        'company_id', 'template_key', 'channel', 'status', 'recipient', 'payload', 'error',
+    ];
