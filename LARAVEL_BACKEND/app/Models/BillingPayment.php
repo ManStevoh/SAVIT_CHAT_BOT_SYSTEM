602d6d89f39e75d7be9ddfa1cad196fa2165fb7e
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:12 +0300

    Add BillingPayment.php (part 2/5) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/BillingPayment.php
+++ LARAVEL_BACKEND/app/Models/BillingPayment.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BillingPayment extends Model
+{
+    protected $fillable = [
+        'company_id', 'subscription_id', 'gateway', 'external_event_id', 'external_payment_id',
+        'amount', 'currency', 'status', 'payment_type', 'metadata', 'paid_at',
