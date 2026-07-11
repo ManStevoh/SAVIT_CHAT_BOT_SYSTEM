72d5f491f9f8c0c2c2e22c664403a38a51e6dd6a
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:01:13 +0300

    Add BusinessGraphController.php (part 2/12) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/BusinessGraphController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/BusinessGraphController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Models\BusinessGraphNode;
+use App\Services\Agent\Graph\BusinessGraphV2Service;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BusinessGraphController extends Controller
+{
