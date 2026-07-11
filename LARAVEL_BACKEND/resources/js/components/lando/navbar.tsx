0d75b895f2dcc0c20d445b6adf5aedc57e8f0dff
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:26:07 +0300

    Add navbar.tsx (part 2/20) in LARAVEL_BACKEND\resources\js\components\lando

--- LARAVEL_BACKEND/resources/js/components/lando/navbar.tsx
+++ LARAVEL_BACKEND/resources/js/components/lando/navbar.tsx
@@ -4,3 +4,9 @@ import { useState } from "react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Menu, X } from "lucide-react"
+import { AppLogoAndName } from "@/components/branding/AppLogoAndName"
+import { cn } from "@/lib/utils"
+import type { CmsLink } from "./types"
+
+interface LandoNavbarProps {
+  links?: CmsLink[]
