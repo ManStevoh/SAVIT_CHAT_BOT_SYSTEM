90459d7b5c61ceb03892b2938e9be6f4a7fb6556
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:28 +0300

    Add MissionControlController.php (part 2/6) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/MissionControlController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/MissionControlController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Services\Agent\MissionControl\MissionControlService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class MissionControlController extends Controller
+{
+    public function index(Request $request, MissionControlService $missionControl): JsonResponse
