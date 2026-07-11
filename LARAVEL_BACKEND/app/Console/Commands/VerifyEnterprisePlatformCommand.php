df4206aeff40c977b84aa4d4aea93cf61a985f34
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:00:15 +0300

    Add VerifyEnterprisePlatformCommand.php (part 2/11) in LARAVEL_BACKEND\app\Console\Commands

--- LARAVEL_BACKEND/app/Console/Commands/VerifyEnterprisePlatformCommand.php
+++ LARAVEL_BACKEND/app/Console/Commands/VerifyEnterprisePlatformCommand.php
@@ -4,3 +4,9 @@ namespace App\Console\Commands;
 
 use App\Services\Agent\AgentToolRegistry;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
+
+class VerifyEnterprisePlatformCommand extends Command
+{
+    protected $signature = 'platform:verify';
+
