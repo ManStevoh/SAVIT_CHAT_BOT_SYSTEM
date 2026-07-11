0ff5d00010bd5d20df0463114558c757dbaf700d
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:18 +0300

    Add BusinessGraphNode.php (part 2/9) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/BusinessGraphNode.php
+++ LARAVEL_BACKEND/app/Models/BusinessGraphNode.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class BusinessGraphNode extends Model
+{
+    public const TYPE_PRODUCT = 'product';
+
