374b39b9b0036f30a5aafe17932735ae8c461ee8
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:57 +0300

    Add WebhookEndpoint.php (part 2/5) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/WebhookEndpoint.php
+++ LARAVEL_BACKEND/app/Models/WebhookEndpoint.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class WebhookEndpoint extends Model
+{
+    protected $fillable = [
+        'company_id', 'url', 'secret', 'events', 'is_active',
