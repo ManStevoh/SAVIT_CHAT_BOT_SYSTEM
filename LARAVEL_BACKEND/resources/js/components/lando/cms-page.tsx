c82356bbaa5f45672848896e4e7941ca166c3910
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:24:37 +0300

    Add cms-page.tsx (part 2/14) in LARAVEL_BACKEND\resources\js\components\lando

--- LARAVEL_BACKEND/resources/js/components/lando/cms-page.tsx
+++ LARAVEL_BACKEND/resources/js/components/lando/cms-page.tsx
@@ -4,3 +4,9 @@ import { Head } from "@inertiajs/react"
 import { usePathname } from "next/navigation"
 import { LandoNavbar } from "./navbar"
 import { LandoFooter } from "./footer"
+import { LandoSectionRenderer } from "./section-renderer"
+import { useCmsPage, useCmsGlobal } from "@/lib/api-hooks"
+import type { CmsLink, CmsSection } from "./types"
+
+interface LandoCmsPageProps {
+  slug: string
