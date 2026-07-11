7a971786500ccce200434116313d9ed999f6e931
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:00:20 +0300

    Add AgentSdkController.php (part 2/3) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/AgentSdkController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/AgentSdkController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\Agent\Platform\MarketplaceModuleService;
+use Illuminate\Http\JsonResponse;
+
+class AgentSdkController extends Controller
+{
+    public function manifest(MarketplaceModuleService $marketplace): JsonResponse
+    {
