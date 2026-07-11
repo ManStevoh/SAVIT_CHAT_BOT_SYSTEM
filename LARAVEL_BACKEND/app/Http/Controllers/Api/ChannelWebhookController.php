4efd0dab7fb821998db60a8b0bc185157a94f0d6
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:00:21 +0300

    Add ChannelWebhookController.php (part 2/25) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/ChannelWebhookController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/ChannelWebhookController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\Agent\Channels\ChannelIngestAuthService;
+use App\Services\Agent\Channels\ChannelReplyDispatcher;
+use App\Services\Agent\Channels\ChatChannel;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
