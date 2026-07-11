31e09ac80efc05af94cfe5ef5082da3675be3131
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:15 +0300

    Add BusinessGraphEdge.php (part 2/6) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/BusinessGraphEdge.php
+++ LARAVEL_BACKEND/app/Models/BusinessGraphEdge.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BusinessGraphEdge extends Model
+{
+    protected $fillable = [
+        'company_id',
+        'from_node_id',
