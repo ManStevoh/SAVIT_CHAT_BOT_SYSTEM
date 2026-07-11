bc53e1c942abaa4cbfd9d3cbc7ca54fd04798482
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:24:40 +0300

    Add cms-page.tsx (part 5/14) in LARAVEL_BACKEND\resources\js\components\lando

--- LARAVEL_BACKEND/resources/js/components/lando/cms-page.tsx
+++ LARAVEL_BACKEND/resources/js/components/lando/cms-page.tsx
@@ -22,3 +22,9 @@ export function LandoCmsPage({ slug, fallbackTitle }: LandoCmsPageProps) {
   const { data: pageData, isLoading } = useCmsPage(slug)
   const { data: globalData } = useCmsGlobal()
 
+  const globalSections = globalData?.sections ?? []
+  const navbarContent = getSectionContent(globalSections, "navbar")
+  const footerContent = getSectionContent(globalSections, "footer")
+
+  const navLinks = (navbarContent.links as CmsLink[] | undefined) ?? []
+  const metaTitle = pageData?.page.metaTitle ?? pageData?.page.title ?? fallbackTitle
