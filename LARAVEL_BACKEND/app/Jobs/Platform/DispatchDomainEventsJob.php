7d984732536590a5897a43b6b9859653cdceb092
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:57 +0300

    Add DispatchDomainEventsJob.php (part 3/3) in LARAVEL_BACKEND\app\Jobs\Platform

--- LARAVEL_BACKEND/app/Jobs/Platform/DispatchDomainEventsJob.php
+++ LARAVEL_BACKEND/app/Jobs/Platform/DispatchDomainEventsJob.php
@@ -10,3 +10,8 @@ class DispatchDomainEventsJob implements ShouldQueue
 {
     use Queueable;
 
+    public function handle(DomainEventDispatcher $dispatcher): void
+    {
+        $dispatcher->processPending(100);
+    }
+}
