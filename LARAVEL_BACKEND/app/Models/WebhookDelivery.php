e784a3713c41aa54ed81c2d11b69b213a9776190
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:55 +0300

    Add WebhookDelivery.php (part 2/5) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/WebhookDelivery.php
+++ LARAVEL_BACKEND/app/Models/WebhookDelivery.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WebhookDelivery extends Model
+{
+    protected $fillable = [
+        'webhook_endpoint_id', 'company_id', 'event_type', 'attempt', 'response_code',
+        'status', 'payload', 'error', 'delivered_at',
