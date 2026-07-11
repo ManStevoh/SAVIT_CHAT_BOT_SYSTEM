5d206255963862ae687aaa8fd1d353786ff4086a
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:45 +0300

    Add InvestigationCase.php (part 2/6) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/InvestigationCase.php
+++ LARAVEL_BACKEND/app/Models/InvestigationCase.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class InvestigationCase extends Model
+{
+    protected $fillable = [
+        'company_id',
+        'owner_analytics_investigation_id',
