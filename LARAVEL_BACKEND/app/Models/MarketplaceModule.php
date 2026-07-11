951cc981a5f26645990c349f72a0ff097ddde4a2
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:47 +0300

    Add MarketplaceModule.php (part 2/6) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/MarketplaceModule.php
+++ LARAVEL_BACKEND/app/Models/MarketplaceModule.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class MarketplaceModule extends Model
+{
+    protected $fillable = [
+        'module_key',
