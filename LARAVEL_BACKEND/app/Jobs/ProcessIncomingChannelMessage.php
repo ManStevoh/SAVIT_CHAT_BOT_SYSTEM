1f4f16de82c7b6bbb67507da45f947fcd421abdc
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:02 +0300

    Add ProcessIncomingChannelMessage.php (part 2/14) in LARAVEL_BACKEND\app\Jobs

--- LARAVEL_BACKEND/app/Jobs/ProcessIncomingChannelMessage.php
+++ LARAVEL_BACKEND/app/Jobs/ProcessIncomingChannelMessage.php
@@ -4,3 +4,9 @@ namespace App\Jobs;
 
 use App\Models\Chat;
 use App\Models\Company;
+use App\Models\Message;
+use App\Services\Agent\Channels\ChatChannel;
+use App\Services\Agent\CommerceAgentReplyService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
