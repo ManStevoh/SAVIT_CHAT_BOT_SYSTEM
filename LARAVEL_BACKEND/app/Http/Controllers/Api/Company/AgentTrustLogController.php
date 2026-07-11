068fa0c75a6bd32a7f6b457e4c2fa5ca2b05f1bb
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:00:32 +0300

    Add AgentTrustLogController.php (part 2/7) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/AgentTrustLogController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/AgentTrustLogController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Models\AgentTrustLog;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AgentTrustLogController extends Controller
+{
+    public function index(Request $request): JsonResponse
