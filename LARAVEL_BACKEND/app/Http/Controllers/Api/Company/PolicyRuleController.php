589a9c850376891f42d6a3a16b6315155ec9cb6d
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:35 +0300

    Add PolicyRuleController.php (part 2/16) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/PolicyRuleController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/PolicyRuleController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Models\CompanyPolicyRule;
+use App\Services\Platform\AuditService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PolicyRuleController extends Controller
+{
