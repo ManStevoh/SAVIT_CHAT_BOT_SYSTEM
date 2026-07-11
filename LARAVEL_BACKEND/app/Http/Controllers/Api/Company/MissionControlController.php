920f4b8bdb86b9465fd8519d57d562d7fa70f2e8
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:29 +0300

    Add MissionControlController.php (part 3/6) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/MissionControlController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/MissionControlController.php
@@ -10,3 +10,9 @@ use Illuminate\Http\Request;
 class MissionControlController extends Controller
 {
     public function index(Request $request, MissionControlService $missionControl): JsonResponse
+    {
+        $company = $request->user()->company;
+        if (! $company) {
+            return response()->json(['message' => 'No company.'], 403);
+        }
+
