40ebe07e114de6c1025e8f050612360a91620617
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:34 +0300

    Add CompanyModuleInstallation.php (part 2/6) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/CompanyModuleInstallation.php
+++ LARAVEL_BACKEND/app/Models/CompanyModuleInstallation.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CompanyModuleInstallation extends Model
+{
+    protected $fillable = [
+        'company_id',
+        'module_key',
