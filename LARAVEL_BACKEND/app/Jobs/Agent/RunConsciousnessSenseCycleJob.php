0f23efd7b10ed6f5f3dd9ead9525a982ff984072
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:54 +0300

    Add RunConsciousnessSenseCycleJob.php (part 2/7) in LARAVEL_BACKEND\app\Jobs\Agent

--- LARAVEL_BACKEND/app/Jobs/Agent/RunConsciousnessSenseCycleJob.php
+++ LARAVEL_BACKEND/app/Jobs/Agent/RunConsciousnessSenseCycleJob.php
@@ -4,3 +4,9 @@ namespace App\Jobs\Agent;
 
 use App\Models\Company;
 use App\Services\Agent\Consciousness\ConsciousnessSenseCycleService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
