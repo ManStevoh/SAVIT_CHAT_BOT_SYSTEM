7afc65a72dd39a5d337dfef22aa0f94e5765f899
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:25 +0300

    Add BusinessTimelineEvent.php (part 2/6) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/BusinessTimelineEvent.php
+++ LARAVEL_BACKEND/app/Models/BusinessTimelineEvent.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BusinessTimelineEvent extends Model
+{
+    protected $fillable = [
+        'company_id',
+        'event_type',
