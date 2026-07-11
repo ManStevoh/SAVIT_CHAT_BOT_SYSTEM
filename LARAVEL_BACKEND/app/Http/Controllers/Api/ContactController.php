f2d6c52abd0ce6ab79b7063726065ef181afa540
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:22:33 +0300

    Add ContactController.php (part 2/7) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/ContactController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/ContactController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\PlatformSetting;
+use App\Services\MailService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
