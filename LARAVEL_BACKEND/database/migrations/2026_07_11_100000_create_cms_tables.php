758bb2f302d50413418cfed5a30b2dea23ef9749
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:23:02 +0300

    Add 2026_07_11_100000_create_cms_tables.php (part 2/7) in LARAVEL_BACKEND\database\migrations

--- LARAVEL_BACKEND/database/migrations/2026_07_11_100000_create_cms_tables.php
+++ LARAVEL_BACKEND/database/migrations/2026_07_11_100000_create_cms_tables.php
@@ -4,3 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('cms_pages', function (Blueprint $table) {
+            $table->id();
