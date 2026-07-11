cb01790faf231d4cd5e985d642e8675daf60a827
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:08:22 +0300

    Update DatabaseSeeder.php in LARAVEL_BACKEND\database\seeders

--- LARAVEL_BACKEND/database/seeders/DatabaseSeeder.php
+++ LARAVEL_BACKEND/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             PlanSeeder::class,
             CompanySeeder::class,
             PaymentGatewaySeeder::class,
+            EnterprisePlatformSeeder::class,
         ]);
     }
 }
