afa661bfbb86c5894a16da2c3ffc859e89450cee
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:03:53 +0300

    Add UsageMeter.php (part 2/4) in LARAVEL_BACKEND\app\Models

--- LARAVEL_BACKEND/app/Models/UsageMeter.php
+++ LARAVEL_BACKEND/app/Models/UsageMeter.php
@@ -4,3 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UsageMeter extends Model
+{
+    protected $fillable = [
+        'company_id', 'meter_key', 'period_start', 'period_end', 'consumed', 'limit_value',
+    ];
