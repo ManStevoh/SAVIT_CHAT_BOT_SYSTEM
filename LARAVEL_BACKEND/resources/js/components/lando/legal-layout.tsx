d339ec54eb7744d30dffdff22fbca30f6ab0b38b
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:25:52 +0300

    Add legal-layout.tsx (part 2/8) in LARAVEL_BACKEND\resources\js\components\lando

--- LARAVEL_BACKEND/resources/js/components/lando/legal-layout.tsx
+++ LARAVEL_BACKEND/resources/js/components/lando/legal-layout.tsx
@@ -4,3 +4,9 @@ import { LandoNavbar } from "@/components/lando/navbar"
 import { LandoFooter } from "@/components/lando/footer"
 import type { CmsLink, CmsSection } from "@/components/lando/types"
 
+function getSectionContent(sections: CmsSection[], key: string) {
+  return sections.find((s) => s.key === key)?.content ?? {}
+}
+
+interface LegalLayoutProps {
+  title: string
