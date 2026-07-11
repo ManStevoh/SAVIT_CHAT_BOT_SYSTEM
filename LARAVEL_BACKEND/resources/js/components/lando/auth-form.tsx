dc805a0463870e023106a15a0bd0e72cf73387db
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 23:24:20 +0300

    Add auth-form.tsx (part 1/9) in LARAVEL_BACKEND\resources\js\components\lando

---
+++ LARAVEL_BACKEND/resources/js/components/lando/auth-form.tsx
@@ -0,0 +1,6 @@
+﻿import { cn } from "@/lib/utils"
+
+export const landoBtnClass =
+  "h-11 w-full rounded-lg bg-[#2563eb] text-white hover:bg-[#1d4ed8] font-medium"
+
+export const landoInputClass =
