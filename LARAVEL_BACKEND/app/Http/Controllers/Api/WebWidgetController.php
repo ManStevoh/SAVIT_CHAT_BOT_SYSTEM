a4686834a02b518a34f7ace912c8621932c42090
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:48 +0300

    Add WebWidgetController.php (part 19/22) in LARAVEL_BACKEND\app\Http\Controllers\Api

--- LARAVEL_BACKEND/app/Http/Controllers/Api/WebWidgetController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/WebWidgetController.php
@@ -106,3 +106,9 @@ class WebWidgetController extends Controller
 
             $validated['message'],
 
+            $validated['name'] ?? 'Web visitor',
+
+            syncReply: true,
+
+        );
+
