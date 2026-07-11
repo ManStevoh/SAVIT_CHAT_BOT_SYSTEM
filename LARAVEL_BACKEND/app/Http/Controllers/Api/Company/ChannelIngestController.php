4768bb87795538ed82df9ea34d049005b95e9924
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:01:26 +0300

    Add ChannelIngestController.php (part 2/16) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/ChannelIngestController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/ChannelIngestController.php
@@ -4,3 +4,9 @@ namespace App\Http\Controllers\Api\Company;
 
 use App\Http\Controllers\Controller;
 use App\Services\Agent\Channels\ChatChannel;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ChannelIngestController extends Controller
+{
