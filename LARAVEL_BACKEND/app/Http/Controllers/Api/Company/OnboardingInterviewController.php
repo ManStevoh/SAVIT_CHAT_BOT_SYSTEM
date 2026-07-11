b4c01529ac26a0ef2358caf73316613bb19668c1
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:02:33 +0300

    Add OnboardingInterviewController.php (part 5/7) in LARAVEL_BACKEND\app\Http\Controllers\Api\Company

--- LARAVEL_BACKEND/app/Http/Controllers/Api/Company/OnboardingInterviewController.php
+++ LARAVEL_BACKEND/app/Http/Controllers/Api/Company/OnboardingInterviewController.php
@@ -22,3 +22,9 @@ class OnboardingInterviewController extends Controller
     public function respond(Request $request, OnboardingInterviewService $interview): JsonResponse
     {
         $company = $request->user()->company;
+        if (! $company) {
+            return response()->json(['message' => 'No company.'], 403);
+        }
+
+        $validated = $request->validate([
+            'sessionId' => 'required|string|max:120',
