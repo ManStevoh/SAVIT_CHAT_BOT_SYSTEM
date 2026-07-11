f1dfddc08b438214cf0d364e0d39cb531b7e941c
ManStevoh <stephenmusyoka207@gmail.com>
2026-07-11 10:00:10 +0300

    Add VerifyAiOrchestrationCommand.php (part 2/11) in LARAVEL_BACKEND\app\Console\Commands

--- LARAVEL_BACKEND/app/Console/Commands/VerifyAiOrchestrationCommand.php
+++ LARAVEL_BACKEND/app/Console/Commands/VerifyAiOrchestrationCommand.php
@@ -4,3 +4,9 @@ namespace App\Console\Commands;
 
 use App\Models\AiModel;
 use App\Services\AI\AiModelResolver;
+use App\Services\AI\AiUseCase;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
+
+class VerifyAiOrchestrationCommand extends Command
+{
