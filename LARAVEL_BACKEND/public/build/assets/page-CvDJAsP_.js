9f5648593adca1bfa3ae76c91b69904966c1a907
staticlumen <staticlumen@gmail.com>
2026-08-25 20:09:37 +0300

    build: compile frontend release bundle

--- LARAVEL_BACKEND/public/build/assets/page-CvDJAsP_.js
+++
@@ -1,1 +0,0 @@
-import{j as t,H as o}from"./app-1_ji7bAs.js";import{A as r}from"./account-profile-panel-cYq7xDoN.js";import"./card-BLfZ9lsf.js";import"./field-DRNG7quC.js";import"./label-xnSEQ-_3.js";import"./index-CEfu0GKs.js";function s(){return t.jsxs(t.Fragment,{children:[t.jsx(o,{title:"My account"}),t.jsx(r,{title:"My account"})]})}export{s as default};
